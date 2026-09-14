018520557472072c7466c654d6528a7b0b3f7b20
parimianusha03-bithub <parimianusha03@gmail.com>
2026-09-14 16:48:30 +0530

    Update game.js

--- game.js
+++ game.js
@@ -62,7 +62,7 @@ return item;
 }
 function createEnemy(){
   const enemy=document.createElement("div");enemy.className="enemy";
-  const img=document.createElement("img");img.src="ENEMY (1).png";img.alt="Festival Mouse";enemy.appendChild(img);
+  const img=document.createElement("img");img.src="ENEMY(2).png";img.alt="Festival Mouse";enemy.appendChild(img);
   enemy.style.left=lanes[Math.floor(Math.random()*3)];enemy.style.top="-70px";objects.appendChild(enemy);return enemy;
 }
 function spawnObject(){
